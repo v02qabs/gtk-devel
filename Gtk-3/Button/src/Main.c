1790bb37392b90b922fb7379f223847ee01fd75c
v02qabs <takesue090@gmail.com>
2020-02-15 22:18:18 +0900

    Window-And-Button-Arm-bin-Main

--- Gtk-3/Button/src/Main.c
+++ Gtk-3/Button/src/Main.c
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
 	GtkWidget *window, *button;
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), "Hello GUI GTK+-3.0");
+	gtk_widget_set_size_request(window, 300, 200);
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 	button = gtk_button_new();
 	gtk_widget_show(button);
 	gtk_container_add(GTK_CONTAINER(window),button);
