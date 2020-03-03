473f283dcc35cf5b9afd7414f22a7d664fc5d3ef
v02qabs <takesue090@gmail.com>
2020-03-03 20:38:31 +0900

    web_load_url_x64

--- webkit/install.sh
+++ webkit/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gcc Main.c -o Main.arm $(pkg-config --cflags --libs gtk+-3.0) $(pkg-config --cflags --libs webkit2gtk-4.0) -I /usr/include/wekitgtk-4.0/
+gcc Main.c -o $1 $(pkg-config --cflags --libs gtk+-3.0) $(pkg-config --cflags --libs webkit2gtk-4.0) -I /usr/include/wekitgtk-4.0/
 
