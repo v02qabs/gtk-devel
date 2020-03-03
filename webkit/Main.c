123b6455666dd31b70940af12d558db9a6a81a79
v02qabs <takesue090@gmail.com>
2020-03-03 20:13:11 +0900

    webkit-add-load-uri

--- webkit/Main.c
+++ webkit/Main.c
@@ -4,10 +4,11 @@
 int main(int argc, char *argv[])
 {
 	gtk_init(&argc, &argv);
-	GtkWidget *window, *webview;
+	GtkWidget *window;
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	webview = webkit_web_view_new();
-	gtk_container_add(GTK_CONTAINER(window), webview);
+	WebKitWebView *webkit = WEBKIT_WEB_VIEW(webkit_web_view_new());
+	webkit_web_view_load_uri(webkit, "http://takos.chobi.net/");
+	gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(webkit));
 	gtk_widget_show_all(window);
 	gtk_main();
 	return 0;
