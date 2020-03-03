25d25d5f813acba9c1895b81dcf9a29642155b8e
v02qabs <takesue090@gmail.com>
2020-03-03 20:59:45 +0900

    utf from euc

--- webkit/install.sh
+++ webkit/install.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-gcc Main.c -o $1 $(pkg-config --cflags --libs gtk+-3.0) $(pkg-config --cflags --libs webkit2gtk-4.0) -I /usr/include/wekitgtk-4.0/
-
+gcc $1 -o $2 $(pkg-config --cflags --libs gtk+-3.0) $(pkg-config --cflags --libs webkit2gtk-4.0) -I /usr/include/wekitgtk-4.0/
