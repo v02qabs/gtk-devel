31ccf450ddd42a6019415f531a6180cd70c8c5fe
v02qabs <takesue090@gmail.com>
2020-08-31 09:10:46 +0900

    del-webkit-all

--- webkit/makefile.sh
+++
@@ -1,2 +0,0 @@
-gcc ./src/$1 -o ./bin/$2 -w $(pkg-config --cflags --libs gtk+-3.0) $(pkg-config --cflags --libs webkit2gtk-4.0)
-
--- webkit/src/remove.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-	remove(argv[1]);
-	return 0;
-}
-
